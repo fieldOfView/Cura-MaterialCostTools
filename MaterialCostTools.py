cba8be7640d9700d23fd1cef5488be0aaa777fea
fieldOfView <aldo@fieldofview.com>
2020-03-08 18:07:05 +0100

    Export all known materials...
    so prices and weights can be edited in an external editor

--- MaterialCostTools.py
+++ MaterialCostTools.py
@@ -13,6 +13,7 @@ from uuid import UUID
 from UM.Extension import Extension
 from UM.Application import Application
 from UM.Logger import Logger
+from UM.Settings.ContainerRegistry import ContainerRegistry
 
 from UM.i18n import i18nCatalog
 catalog = i18nCatalog("cura")
@@ -30,11 +31,11 @@ class MaterialCostTools(Extension, QObject,):
         if sys.platform == "linux" and "KDE_FULL_SESSION" in os.environ:
             self._dialog_options |= QFileDialog.DontUseNativeDialog
 
+        self.setMenuName(catalog.i18nc("@item:inmenu", "Material Cost Tools"))
 
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Import weights && prices..."), self.importData)
         self.addMenuItem(catalog.i18nc("@item:inmenu", "Export weights && prices..."), self.exportData)
 
-        self.setMenuName(catalog.i18nc("@item:inmenu", "Material Cost Tools"))
 
     def exportData(self):
         file_name = QFileDialog.getSaveFileName(
@@ -57,19 +58,39 @@ class MaterialCostTools(Extension, QObject,):
             Logger.logException("e", "Could not load material settings from preferences")
             return
 
+        container_registry = ContainerRegistry.getInstance()
+        materials = [
+            {
+                "guid": m["GUID"],
+                "material": m["material"],
+                "brand": m.get("brand",""),
+                "name": m["name"],
+                "spool_weight": material_settings.get(m["GUID"], {}).get("spool_weight", ""),
+                "spool_cost": material_settings.get(m["GUID"], {}).get("spool_cost", "")
+            }
+            for m in container_registry.findInstanceContainersMetadata(type = "material")
+            if m["id"] == m["base_file"] and "brand" in m
+        ].sort(key = lambda k: (k["brand"], k["material"], k["name"]))
+
         try:
             with open(file_name, 'w', newline='') as csv_file:
                 csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                csv_writer.writerow(["guid", "weight (g)", "cost (%s)" % self._preferences.getValue("cura/currency")])
+                csv_writer.writerow(["guid", "weight (g)", "cost (%s), name" % self._preferences.getValue("cura/currency")])
 
-                for (guid, data) in material_settings.items():
+                for material in materials:
                     try:
-                        csv_writer.writerow([guid, data["spool_weight"], data["spool_cost"]])
+                        csv_writer.writerow([
+                            material["guid"],
+                            material["spool_weight"],
+                            material["spool_cost"],
+                            "%s %s" % (material["brand"], material["name"])
+                        ])
                     except:
                         continue
         except:
             Logger.logException("e", "Could not export settings to the selected file")
             return
+
 
     def importData(self):
         file_name = QFileDialog.getOpenFileName(
@@ -102,7 +123,7 @@ class MaterialCostTools(Extension, QObject,):
                         header = row
                     else:
                         try:
-                            (guid, weight, cost) = row
+                            (guid, weight, cost) = row[0:3]
                         except:
                             Logger.log("e", "Row does not have enough data: %s" % row)
                             continue
